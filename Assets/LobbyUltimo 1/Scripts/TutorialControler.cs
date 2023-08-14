d80790efbdf7051942794986388982511f06ec7e
santgame7 <jhoseph.arevalo@soy.sena.edu.co>
2023-08-14 09:45:05 -0500

    9:36/14
    correcion de errores

---
+++ Assets/LobbyUltimo 1/Scripts/TutorialControler.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialControler : MonoBehaviour
+{
+    [SerializeField] GameObject tutorial;
+    [SerializeField] GameObject insTutorial;
+    [SerializeField] FirstPersonLook _jugador;
+
+    [SerializeField] Image Estamina;
+    [SerializeField] Image Cansancio;
+    [SerializeField] Image botonInteraccion;
+
+    private void Start()
+    {
+        _jugador.enabled = false;
+
+        Estamina.enabled = false;
+        Cansancio.enabled = false;
+        botonInteraccion.enabled = false;
+        insTutorial.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+    public void Tutorial()
+    {
+        Destroy(tutorial);
+        _jugador.enabled = true;
+        insTutorial.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = false;
+    }
+}
---
+++ Assets/Mini First Person Controller/ObjectPicker.cs
@@ -0,0 +1,152 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectPicker : MonoBehaviour
+{
+    public float pickupDistance = 2f;
+    public KeyCode pickupKey = KeyCode.E;
+    public GameObject linterna;
+    public Image interactionImage;
+    public GameObject puerta;
+
+    private Transform pickedObject;
+    private Camera mainCamera;
+    private bool isHolding = false;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        interactionImage.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickupDistance))
+        {
+            if (hit.collider.CompareTag("Pickable") || hit.collider.CompareTag("Linterna") || hit.collider.CompareTag("Libro") || hit.collider.CompareTag("Puerta"))
+            {
+                interactionImage.gameObject.SetActive(true);
+            }
+            else
+            {
+                interactionImage.gameObject.SetActive(false);
+            }
+
+            if (pickedObject == null)
+            {
+                if (Input.GetKeyDown(pickupKey))
+                {
+                    TryPickObject();
+                }
+            }
+            else
+            {
+                if (Input.GetKeyDown(pickupKey))
+                {
+                    TryDropObject();
+                }
+                else
+                {
+                    MovePickedObject();
+                }
+            }
+        }
+        else
+        {
+            interactionImage.gameObject.SetActive(false);
+
+            if (pickedObject != null)
+            {
+                if (Input.GetKeyDown(pickupKey))
+                {
+                    TryDropObject();
+                }
+                else
+                {
+                    MovePickedObject();
+                }
+            }
+        }
+    }
+
+    private void TryPickObject()
+    {
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0f));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, pickupDistance))
+        {
+            if (hit.collider.CompareTag("Pickable"))
+            {
+                pickedObject = hit.transform;
+                isHolding = true;
+            }
+            else if (hit.collider.CompareTag("Linterna") && !isHolding)
+            {
+                Destroy(hit.collider.gameObject);
+                ActivateNewObject();
+            }
+            if (hit.collider.CompareTag("Puerta"))
+            {
+                TryToggleDoor(hit.collider.gameObject);
+            }
+        }
+    }
+
+    private void TryToggleDoor(GameObject door)
+    {
+        Animator doorAnimator = door.GetComponent<Animator>();
+        if (doorAnimator != null)
+        {
+            doorAnimator.SetBool("Abrir", !doorAnimator.GetBool("Abrir"));
+        }
+    }
+
+    private void MovePickedObject()
+    {
+        Vector3 newPosition = mainCamera.transform.position + mainCamera.transform.forward * pickupDistance;
+        pickedObject.position = newPosition;
+    }
+
+    private void TryDropObject()
+    {
+        ChangeColorOnCollision changeColorScript = pickedObject.GetComponent<ChangeColorOnCollision>();
+        if (changeColorScript && (!changeColorScript.IsRed() || !IsCollidingWithRedObjects()))
+        {
+            DropObject();
+            Rigidbody pickedObjectRigidbody = pickedObject.GetComponent<Rigidbody>();
+            if (pickedObjectRigidbody != null)
+            {
+                pickedObjectRigidbody.velocity = Vector3.zero;
+                pickedObjectRigidbody.angularVelocity = Vector3.zero;
+            }
+        }
+    }
+
+    private void DropObject()
+    {
+        pickedObject = null;
+        isHolding = false;
+    }
+
+    private bool IsCollidingWithRedObjects()
+    {
+        Collider[] colliders = Physics.OverlapSphere(pickedObject.position, 0.5f);
+        foreach (var collider in colliders)
+        {
+            if (collider.CompareTag("Cosa"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ActivateNewObject()
+    {
+        linterna.SetActive(true);
+    }
+}
